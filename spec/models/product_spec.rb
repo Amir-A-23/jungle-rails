609ef1b60bd2a98578d3b92d13ba935df33ac098
Amir <amir.a.23@hotmail.com>
2022-01-13 02:51:47 +0000

    Add test to check if valid without name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
     it "tests that all fields are saved sucessfully" do
+
       @category = Category.new
       @category.id = 4
       @category.name = 'Misc'
@@ -16,12 +17,30 @@ RSpec.describe Product, type: :model do
       @product.category_id = @category
 
       @category.products = [@product]
-
       @product.save
       
       expect(@product).to be_valid
     end
 
+
+    it "tests that saved sucessfully without name" do
+
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Misc'
+      
+      @product = Product.new
+      @product.name = nil
+      @product.price = 999
+      @product.quantity = 999
+      @product.category_id = @category
+
+      @category.products = [@product]
+      @product.save
+      
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
     
 
   end
